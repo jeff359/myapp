c7df05eeff62a07f5ff29de946d535d622a13084
Jeffrey Muriithi <jefahk@gmail.com>
2022-08-27 14:36:24 +0300

    commit3

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import NextImage from "next/image";
+import NextLink from "next/link";
 import {
     Box,
     List,
@@ -17,6 +18,38 @@ import {
     MdFavorite
 } from "react-icons/md";
 
+const navMenu = [
+    {
+        name: "Home",
+        icon: MdHome,
+        route: "/"
+    },
+    {
+        name: "Search",
+        icon: MdSearch,
+        route: "/search"
+    },
+    {
+        name: "Your Library",
+        icon: MdLibraryMusic,
+        route: "/library"
+    },
+]
+const musicMenu = [
+    {
+        name: "Create Playlist",
+        icon: MdPlaylistAdd,
+        route: "/"
+    },
+    {
+        name: "Favorites",
+        icon: MdFavorite,
+        route: "/favorites"
+    },
+]
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i+1}`)
+
 const Sidebar = () => {
 return (
     <Box 
@@ -26,10 +59,44 @@ return (
     paddingX = "5px" 
     color = "gray"
     >
-    <Box paddingY = "20px">
+    <Box paddingY = "20px" height = "100%">
         <Box width = "120px" marginBottom = "20px" paddingX = "20px">
             <NextImage src = "/logo.svg" height = {60} width = {120}/>
         </Box>
+        <Box marginBottom = "20px">
+            <List spacing = {2}>
+                {navMenu.map(menu=>(
+                    <ListItem paddingX = "20px" fontSize = "16px" key = {menu.name}>
+                    <LinkBox>
+                    <NextLink href = {menu.route} passHref>
+                    <LinkOverlay>
+                    <ListIcon as = {menu.icon} color = "white" marginRight = "20px"/>
+                    {menu.name}
+                    </LinkOverlay>
+                    </NextLink>
+                    </LinkBox>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+        <Box marginTop = "20px">
+            <List spacing = {2}>
+                {musicMenu.map(menu => (
+                    <ListItem paddingX = "20px" fontSize = "16px" key = {menu.name}>
+                        <LinkBox>
+                    <NextLink href = {menu.route} passHref>
+                    <LinkOverlay>
+                    <ListIcon as = {menu.icon} color = "white" marginRight = "20px"/>
+                    {menu.name}
+                    </LinkOverlay>
+                    </NextLink>
+                    </LinkBox>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+        <Divider color = "gray.800"/>
+        <Box height = "66%" overflowY = "auto" padding Y = "20px"></Box>
     </Box>
     </Box>
 )
